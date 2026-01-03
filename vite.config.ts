994a24afe9b4d28f16d1785c79d758796715aec2
mayowa-id <idowumayowa02@gmail.com>
2026-01-03 11:13:25 +0100

    moved index file to root

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
   publicDir: 'public',
   server: {
     port: 3000,
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html')
-    }
   },
 })
